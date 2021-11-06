88fae625418de7091a89b4084cbf6cbffb8f94fd
Christian <chris23681@gmail.com>
2021-11-06 14:56:53 -0700

    Adding answers to exercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(word, amount) {
+    together = '';
+    let i;
+    if (amount < 0) {
+        return 'ERROR';
+    }
+    for (i=0; i < amount; i++) {
+        together += word;
+    }
+    return together;
 };
 
 // Do not edit below this line
