c6c250c4f44c0c75095147b1bcac756725fc86b6
Christian <chris23681@gmail.com>
2021-11-06 15:41:29 -0700

    Adding Ex 4 answer

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
 const removeFromArray = function() {
-
+    let args = Array.from(arguments);
+    let startingArray = args[0];
+    let toRemove = args.slice(1);
+    let amount = toRemove.length;
+    for (let i = 0; i < amount; i++) {
+        let index = startingArray.indexOf(toRemove[i]);
+        if (index > -1) {
+            startingArray.splice(index, 1);
+        }
+    }
+    return startingArray;
 };
 
 // Do not edit below this line
