5a1195db2573940370cea9dfa6a6870430ecb3f7
Christian <chris23681@gmail.com>
2021-11-06 19:00:55 -0700

    Added Ex 7 answers

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let celsius;
+  celsius = (temp - 32) * (5/9);
+  rounded = Math.round(celsius * 10) / 10;
+  return rounded;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let farenheit;
+  farenheit = (temp * 9 / 5) + 32;
+  rounded = Math.round(farenheit * 10) / 10;
+  return rounded;
 };
 
 // Do not edit below this line
