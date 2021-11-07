d362882804bc8bf0223373fd4d541d7da63806ef
Christian <chris23681@gmail.com>
2021-11-06 22:47:45 -0700

    Added JS for Drum Kit

--- js-insertion/script.js
+++ js-insertion/script.js
@@ -23,3 +23,17 @@ div.appendChild(p2);
 div.style.borderColor = 'black';
 div.style.backgroundColor = 'pink';
 container.appendChild(div);
+
+const btn = document.querySelector('#btn');
+btn.onclick = () => alert("Hello World");
+
+const btn2 = document.querySelector('#btn2');
+btn2.addEventListener('click', () => {
+    alert('Hello World');
+});
+
+const btn3 = document.querySelector("#btn3");
+btn3.addEventListener('click', function (e) {
+    console.log(e.target);
+    e.target.style.background = 'blue';
+});
