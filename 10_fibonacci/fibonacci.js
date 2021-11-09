fcd163b0b24dc693febf1547289871cd42364be1
Christian <chris23681@gmail.com>
2021-11-09 12:12:33 -0800

    Added Ex 10 answers

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    let fib1 = 0;
+    let fib2 = 1;
+    number = parseInt(number);
+    if (!number) return "OOPS";
+    if (number < 0) return 'OOPS';
+    if (number === 0) return fib1;
+    for (let i = 0; i < number; i++) {
+        let fib3 = fib1 + fib2;
+        fib1 = fib2;
+        fib2 = fib3;
+    }
+    return fib1;
 };
 
 // Do not edit below this line
