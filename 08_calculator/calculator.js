7cb01d89dd8098e8a6d3f7c982f154b9cd44118a
Christian <chris23681@gmail.com>
2021-11-09 09:27:11 -0800

    Added 8 and 9 answers

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return parseInt(a) + parseInt(b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return parseInt(a) - parseInt(b);
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	const sumReducer = (previousValue, currentValue) => previousValue + currentValue;
+  if (a.length === 0) return 0;
+  return a.reduce(sumReducer);
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  const multReducer = (previousValue, currentValue) => previousValue * currentValue;
+  return a.reduce(multReducer);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let total = 1;
+	for (let i = 1; i <= a; i++) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let punctuationless = string.replace(/[^\w]/g,"");
+    punctuationless = punctuationless.toLowerCase();
+    let stringArray = Array.from(punctuationless);
+    for (let i = 0; i <= punctuationless.length; i++) {
+        if (stringArray[i] !== stringArray[punctuationless.length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
